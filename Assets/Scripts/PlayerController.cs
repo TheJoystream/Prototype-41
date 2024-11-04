6fd966ce3cfc46b723fd177148f28647af30dd7a
TheJoystream <Dean.millen88@gmail.com>
2024-11-04 21:57:17 +0000

    Create

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject focalPoint;
+    public bool hasPowerUp = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +21,10 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Poweup"))
+        { Destroy(other.gameObject); }
     }
 }
